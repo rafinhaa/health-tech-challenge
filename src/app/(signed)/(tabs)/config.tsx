3c02e0d4ff47e43497ec580d694e3869df84918f
rafinhaa <rafinhaa@me.com>
2024-11-05 13:59:39 -0300

    feat: screen config

--- src/app/(signed)/(tabs)/config.tsx
+++ src/app/(signed)/(tabs)/config.tsx
@@ -1,17 +1,43 @@
-import { StyleSheet, Text, View } from "react-native"
+import { useTranslation } from "react-i18next"
+
+import { Avatar, AvatarFallbackText, AvatarImage } from "@/components/ui/avatar"
+import { Box } from "@/components/ui/box"
+import { Button, ButtonText } from "@/components/ui/button"
+import { Heading } from "@/components/ui/heading"
+import { SettingsButton } from "@/components/ui/settings-button"
+import { Text } from "@/components/ui/text"
 
 export default function Config() {
+  const { t } = useTranslation()
+
   return (
-    <View style={styles.container}>
-      <Text>Config</Text>
-    </View>
+    <Box className="flex-1 bg-white ">
+      <Box className="w-full h-1/4 bg-blue-600" />
+      <Avatar size="2xl" className="-mt-[50px] self-center">
+        <AvatarFallbackText>Jane Doe</AvatarFallbackText>
+        <AvatarImage
+          source={{
+            uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          }}
+        />
+      </Avatar>
+      <Box className="flex-1 px-5 items-center">
+        <Heading className="color-black font-inter600" size="2xl">
+          Jane Doe
+        </Heading>
+        <Text className="color-shadow-secondary font-inter400" size="2xl">
+          jane.doe@example.com
+        </Text>
+        <Box className="gap-4 mt-8">
+          <SettingsButton label={t("configScreen.myData")} icon="account" />
+          <SettingsButton label={t("configScreen.notifications")} icon="bell" />
+          <SettingsButton label={t("configScreen.terms")} icon="information" />
+        </Box>
+
+        <Button className="bg-error-500 mt-8 w-full" size="lg">
+          <ButtonText>{t("configScreen.exit")}</ButtonText>
+        </Button>
+      </Box>
+    </Box>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
