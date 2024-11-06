9e90a82d4a1fe08448599fe00efbd8b63e38ae56
rafinhaa <rafinhaa@me.com>
2024-11-06 11:32:08 -0300

    feat: form validartion login screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
-import { router } from "expo-router"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useState } from "react"
+import { Controller, useForm } from "react-hook-form"
 import { useTranslation } from "react-i18next"
 import { KeyboardAvoidingView, Platform } from "react-native"
+import { z } from "zod"
 
 import { Box } from "@/components/ui/box"
-import { Button, ButtonText } from "@/components/ui/button"
+import { Button, ButtonSpinner, ButtonText } from "@/components/ui/button"
 import {
   FormControl,
   FormControlError,
@@ -18,14 +20,26 @@ import Icon from "@/components/ui/icon"
 import { Input, InputField, InputSlot } from "@/components/ui/input"
 import { Text } from "@/components/ui/text"
 
+const loginSchema = z.object({
+  username: z.string().min(1),
+  password: z.string().min(1),
+})
+
+type LoginSchema = z.infer<typeof loginSchema>
+
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false)
 
   const { t } = useTranslation()
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+  })
 
-  const handlePressLogin = () => {
-    router.replace("/(signed)")
-  }
+  const handlePressLogin = async (_data: LoginSchema) => {}
 
   const handlePressShowPassword = () => {
     setShowPassword(!showPassword)
@@ -47,69 +61,95 @@ export default function Login() {
           </Text>
 
           <Box className="w-full bg-white rounded-2xl border border-shadow-primary px-6 py-[50px] gap-6">
-            <FormControl>
-              <FormControlLabel>
-                <FormControlLabelText>
-                  {t("loginScreen.username")}
-                </FormControlLabelText>
-              </FormControlLabel>
-              <Input className="my-1">
-                <InputField type="text" />
-              </Input>
-              <FormControlError>
-                <FormControlErrorIcon
-                  as={() => (
-                    <Icon
-                      name="alert-circle-outline"
-                      size={20}
-                      colorClassName="color-indicator-error"
+            <Controller
+              control={control}
+              name="username"
+              render={({ field: { onChange, value }, formState }) => (
+                <FormControl isInvalid={!!formState.errors.username}>
+                  <FormControlLabel>
+                    <FormControlLabelText>
+                      {t("loginScreen.username")}
+                    </FormControlLabelText>
+                  </FormControlLabel>
+                  <Input className="my-1">
+                    <InputField
+                      type="text"
+                      value={value}
+                      onChangeText={onChange}
+                      autoCapitalize="none"
+                      autoCorrect={false}
                     />
-                  )}
-                />
-                <FormControlErrorText>
-                  {t("common.requiredField")}
-                </FormControlErrorText>
-              </FormControlError>
-            </FormControl>
-            <FormControl>
-              <FormControlLabel>
-                <FormControlLabelText>
-                  {t("loginScreen.password")}
-                </FormControlLabelText>
-              </FormControlLabel>
-              <Input className="my-1">
-                <InputField type={showPassword ? "text" : "password"} />
-                <InputSlot className="px-4">
-                  <Icon
-                    name={showPassword ? "eye-outline" : "eye-off-outline"}
-                    size={20}
-                    colorClassName="color-gray-600"
-                    onPress={handlePressShowPassword}
-                  />
-                </InputSlot>
-              </Input>
-              <FormControlError>
-                <FormControlErrorIcon
-                  as={() => (
-                    <Icon
-                      name="alert-circle-outline"
-                      size={20}
-                      colorClassName="color-indicator-error"
+                  </Input>
+                  <FormControlError>
+                    <FormControlErrorIcon
+                      as={() => (
+                        <Icon
+                          name="alert-circle-outline"
+                          size={20}
+                          colorClassName="color-indicator-error"
+                        />
+                      )}
                     />
-                  )}
-                />
-                <FormControlErrorText>
-                  {t("common.requiredField")}
-                </FormControlErrorText>
-              </FormControlError>
-            </FormControl>
+                    <FormControlErrorText>
+                      {t("common.requiredField")}
+                    </FormControlErrorText>
+                  </FormControlError>
+                </FormControl>
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value }, formState }) => (
+                <FormControl isInvalid={!!formState.errors.password}>
+                  <FormControlLabel>
+                    <FormControlLabelText>
+                      {t("loginScreen.password")}
+                    </FormControlLabelText>
+                  </FormControlLabel>
+                  <Input className="my-1">
+                    <InputField
+                      type={showPassword ? "text" : "password"}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                    <InputSlot className="px-4">
+                      <Icon
+                        name={showPassword ? "eye-outline" : "eye-off-outline"}
+                        size={20}
+                        colorClassName="color-gray-600"
+                        onPress={handlePressShowPassword}
+                      />
+                    </InputSlot>
+                  </Input>
+                  <FormControlError>
+                    <FormControlErrorIcon
+                      as={() => (
+                        <Icon
+                          name="alert-circle-outline"
+                          size={20}
+                          colorClassName="color-indicator-error"
+                        />
+                      )}
+                    />
+                    <FormControlErrorText>
+                      {t("common.requiredField")}
+                    </FormControlErrorText>
+                  </FormControlError>
+                </FormControl>
+              )}
+            />
             <Button
               className="bg-blue-600 data-[hover=true]:bg-blue-400 data-[active=true]:bg-blue-400"
-              onPress={handlePressLogin}
+              onPress={handleSubmit(handlePressLogin)}
             >
-              <ButtonText className="text-white">
-                {t("common.enter")}
-              </ButtonText>
+              {isSubmitting ? (
+                <ButtonSpinner color="white" />
+              ) : (
+                <ButtonText className="text-white">
+                  {t("common.enter")}
+                </ButtonText>
+              )}
             </Button>
           </Box>
         </Box>
